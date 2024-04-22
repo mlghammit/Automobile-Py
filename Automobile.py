11dc77ea74939297b243c4813b433e027c992c76
mlghammit <58157953+mlghammit@users.noreply.github.com>
2024-04-21 20:55:52 -0400

    Create Automobile.py

---
+++ Automobile.py
@@ -0,0 +1,22 @@
+lass Automobile():
+    def __init__(self, ndoors=0, clr=""):
+        self._doors = ndoors
+        self._colour = clr
+
+    def printDoors(self):
+        return "Number of doors is: " + str(self._doors)
+
+    def printColour(self):
+        return "Colour is: " + self._colour
+
+    def display(self):
+        return "Car is: " + str(self._doors) + " doors, " + self._colour
+
+class SportsCar(Automobile):
+    def __init__(self, ndoors=0, clr="", eng=0): #Should have same parameters, should be self first
+        super().__init__(ndoors, clr)
+        self._engine = eng
+
+    def display(self):
+        return super().display(), "with " + str(self._engine) + " hp"
+
